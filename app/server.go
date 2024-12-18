15976f7b10821782b26534ee60868e099fc794d0
beshubh <be.shubh42@gmail.com>
2024-12-19 00:25:47 +0530

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -34,6 +34,29 @@ func main() {
 	}
 }
 
+type Message struct {
+	messageSize   uint32
+	apiKey        uint16
+	apiVersion    uint16
+	correlationId uint32
+}
+
+func read(conn net.Conn) (Message, error) {
+	buffer := make([]byte, 1024)
+	_, err := conn.Read(buffer)
+	if err != nil {
+		fmt.Println("Error reading from connection: ", err.Error())
+		return Message{}, err
+	}
+	message := Message{
+		messageSize:   binary.BigEndian.Uint32(buffer[0:4]),
+		apiKey:        binary.BigEndian.Uint16(buffer[4:6]),
+		apiVersion:    binary.BigEndian.Uint16(buffer[6:8]),
+		correlationId: binary.BigEndian.Uint32(buffer[8:12]),
+	}
+	return message, nil
+}
+
 func send(conn net.Conn, message *[]byte) error {
 	binary.Write(conn, binary.BigEndian, int32(len(*message)))
 	binary.Write(conn, binary.BigEndian, message)
@@ -41,41 +64,42 @@ func send(conn net.Conn, message *[]byte) error {
 }
 
 func handleConnection(conn net.Conn) {
-	buffer := make([]byte, 1024)
-	_, err := conn.Read(buffer)
-	if err != nil {
-		fmt.Println("Error reading from connection: ", err.Error())
-		return
-	}
-	_ = binary.BigEndian.Uint16(buffer[0:4])
-	reqApiKey := binary.BigEndian.Uint16(buffer[4:6])
-	reqApiVersion := binary.BigEndian.Uint16(buffer[6:8])
-	correlationId := binary.BigEndian.Uint32(buffer[8:12])
-
-	errorCode := 0
-	if reqApiVersion > 4 {
-		errorCode = 35
-	}
-
-	fmt.Println("Got request from client: ", reqApiKey, reqApiVersion, correlationId)
-
-	if reqApiKey == 18 {
-		fmt.Println("ApiVersions request")
-		response := make([]byte, 19)
-		binary.BigEndian.PutUint32(response[0:], correlationId)
-		binary.BigEndian.PutUint16(response[4:], uint16(errorCode))
-		response[6] = 2
-		// api version entry
-		binary.BigEndian.PutUint16(response[7:], 18)
-		binary.BigEndian.PutUint16(response[9:], 3)
-		binary.BigEndian.PutUint16(response[11:], 4)
-		response[13] = 0
-		binary.BigEndian.PutUint32(response[14:], 0)
-		response[18] = 0
-		err = send(conn, &response)
+	for {
+		buffer := make([]byte, 1024)
+		_, err := conn.Read(buffer)
 		if err != nil {
-			fmt.Println("Error sending response: ", err.Error())
+			fmt.Println("Error reading from connection: ", err.Error())
 			return
+		}
+		message, err := read(conn)
+		if err != nil {
+			fmt.Println("Error reading message: ", err.Error())
+			return
+		}
+
+		errorCode := 0
+		if message.apiVersion > 4 {
+			errorCode = 35
+		}
+
+		if message.apiKey == 18 {
+			fmt.Println("ApiVersions request")
+			response := make([]byte, 19)
+			binary.BigEndian.PutUint32(response[0:], message.correlationId)
+			binary.BigEndian.PutUint16(response[4:], uint16(errorCode))
+			response[6] = 2
+			// api version entry
+			binary.BigEndian.PutUint16(response[7:], 18)
+			binary.BigEndian.PutUint16(response[9:], 3)
+			binary.BigEndian.PutUint16(response[11:], 4)
+			response[13] = 0
+			binary.BigEndian.PutUint32(response[14:], 0)
+			response[18] = 0
+			err = send(conn, &response)
+			if err != nil {
+				fmt.Println("Error sending response: ", err.Error())
+				return
+			}
 		}
 	}
 }
